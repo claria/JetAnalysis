c59f32cdd74c82a6436d5f8f6ee6005bc69cdcfb
Georg <sieber@cern.ch>
2014-04-10 13:08:09 +0200

    added DiJetsPtFilter

---
+++ DijetAna/interface/DiJetsPtFilter.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Artus/Core/interface/Cpp11Support.h"
+#include "Artus/Core/interface/FilterBase.h"
+
+#include "JetTypes.h"
+
+class DiJetsPtFilter: public JetFilterBase {
+	public:
+
+		virtual ~DiJetsPtFilter() {
+		}
+
+		virtual std::string GetFilterId() const {
+			return "DiJetsPtFilter";
+		}
+
+		virtual void InitGlobal(JetGlobalSettings const& globalSettings) {
+			minJetPtCut = globalSettings.GetMinJetPtCut();
+		}
+
+		virtual bool DoesEventPassGlobal(JetEvent const& event,
+				JetProduct const& product,
+				JetGlobalSettings const& globalSettings) const {
+
+			bool pass = true;
+
+			if (product.m_validJets.at(0)->p4.Pt() < minJetPtCut ||
+				product.m_validJets.at(1)->p4.Pt() < minJetPtCut) {
+				pass = false;
+			}
+			return pass;
+		}
+
+	private:
+		double minJetPtCut;
+};
+
