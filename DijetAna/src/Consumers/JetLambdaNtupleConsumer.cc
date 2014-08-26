3abb08b6bf2ab6c1463dc55ea1cab39fabe9f5eb
Georg <sieber@cern.ch>
2014-08-26 16:51:49 +0200

    commented out not neccesary stuff

--- DijetAna/src/Consumers/JetLambdaNtupleConsumer.cc
+++ DijetAna/src/Consumers/JetLambdaNtupleConsumer.cc
@@ -20,16 +20,16 @@ void JetLambdaNtupleConsumer::Init(Pipeline<JetTypes> * pipeline)
 	{
 		return event.m_eventMetadata->nEvent;
 	};
-	LambdaNtupleConsumer<JetTypes>::Quantities["puweight"] = [](event_type const& event, product_type const& product) {
-		return SafeMap::GetWithDefault(product.m_weights, std::string("puWeight"), 1.0);
-	};
-		LambdaNtupleConsumer<JetTypes>::Quantities["weight"] = [pipeline](event_type const& event, product_type const& product) {
+	//LambdaNtupleConsumer<JetTypes>::Quantities["puweight"] = [](event_type const& event, product_type const& product) {
+	//	return SafeMap::GetWithDefault(product.m_weights, std::string("puWeight"), 1.0);
+	//};
+	LambdaNtupleConsumer<JetTypes>::Quantities["weight"] = [pipeline](event_type const& event, product_type const& product) {
 		return SafeMap::GetWithDefault(product.m_weights, pipeline->GetSettings().GetEventWeight(), 1.0);
 	};
-	LambdaNtupleConsumer<JetTypes>::Quantities["jet1_pt"] = LambdaNtupleConsumer<JetTypes>::Quantities["leadingJetPt"];
+	LambdaNtupleConsumer<JetTypes>::Quantities["jet1_pt"]  = LambdaNtupleConsumer<JetTypes>::Quantities["leadingJetPt"];
 	LambdaNtupleConsumer<JetTypes>::Quantities["jet1_eta"] = LambdaNtupleConsumer<JetTypes>::Quantities["leadingJetEta"];
 	LambdaNtupleConsumer<JetTypes>::Quantities["jet1_phi"] = LambdaNtupleConsumer<JetTypes>::Quantities["leadingJetPhi"];
-	LambdaNtupleConsumer<JetTypes>::Quantities["jet2_pt"] = LambdaNtupleConsumer<JetTypes>::Quantities["trailingJetPt"];
+	LambdaNtupleConsumer<JetTypes>::Quantities["jet2_pt"]  = LambdaNtupleConsumer<JetTypes>::Quantities["trailingJetPt"];
 	LambdaNtupleConsumer<JetTypes>::Quantities["jet2_eta"] = LambdaNtupleConsumer<JetTypes>::Quantities["trailingJetEta"];
 	LambdaNtupleConsumer<JetTypes>::Quantities["jet2_phi"] = LambdaNtupleConsumer<JetTypes>::Quantities["trailingJetPhi"];
 
