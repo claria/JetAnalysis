2aeae3736148b690e98a1e5b1493226b670013b6
Georg <sieber@cern.ch>
2014-09-08 10:01:05 +0200

    new consumers for trigger eff.

--- DijetAna/interface/Consumers/TriggerHistogramFitConsumer.h
+++ DijetAna/interface/Consumers/TriggerHistogramFitConsumer.h
@@ -1,70 +1,23 @@
 
 #pragma once
 
-//#include <boost/scoped_ptr.hpp>
+#include "JetAnalysis/DijetAna/interface/JetTypes.h"
 
-#include <TH1.h>
-#include "TROOT.h"
-
-#include "Artus/Core/interface/Cpp11Support.h"
-#include "Artus/Utility/interface/RootFileHelper.h"
-
-#include "Artus/Consumer/interface/LambdaNtupleConsumer.h"
-
-
-template < class TTypes >
-class KappaTriggerFitHistogramHistogramConsumer: public ConsumerBase< TTypes > {
+class TriggerHistogramFitConsumer: public ConsumerBase<JetTypes> {
 public:
 
-	typedef typename TTypes::event_type event_type;
-	typedef typename TTypes::product_type product_type;
-	typedef typename TTypes::setting_type setting_type;
-	
 	virtual std::string GetConsumerId() const
-		ARTUS_CPP11_OVERRIDE
 	{
-		return "KappaTriggerResultsHistogramConsumer";
+		return "TriggerHistogramFitConsumer";
 	}
 
-	virtual void Init(Pipeline<TTypes> * pipeline) ARTUS_CPP11_OVERRIDE
-	{
-		ConsumerBase<TTypes>::Init( pipeline );
-		m_triggerPaths = this->GetPipelineSettings().GetHltPaths();
-	
-		RootFileHelper::SafeCd(this->GetPipelineSettings().GetRootOutFile(),
-		                       this->GetPipelineSettings().GetRootFileFolder());
-
-		for (std::string hltPath : m_triggerPaths) {
-			m_triggerResultHists.push_back(new TH1F(hltPath.c_str(), hltPath.c_str(), 100., 0., 1000.));
-		}
-
-
-	}
-
-	virtual void ProcessFilteredEvent(event_type const& event,
-	                          product_type const& product)
-	{
-		for(std::vector<std::string>::size_type i = 0; i != m_triggerPaths.size(); i++) {
-			if (event.m_eventMetadata->hltFired(product.m_hltInfo->getHLTName(m_triggerPaths[i]), event.m_lumiMetadata)){
-				m_triggerResultHists[i]->Fill((LambdaNtupleConsumer<TTypes>::Quantities[this->GetPipelineSettings().GetTriggerEfficiencyQuantity()])(event, product));
-			}
-		}
-	}
-
-	virtual void Finish() ARTUS_CPP11_OVERRIDE {
-		
-		// save histograms
-		RootFileHelper::SafeCd(this->GetPipelineSettings().GetRootOutFile(),
-		                       this->GetPipelineSettings().GetRootFileFolder());
-		
-		for (TH1F* hist : m_triggerResultHists) {
-			hist->Write(hist->GetName());
-		}
-	
-	}
+	virtual void Init(Pipeline<JetTypes> * pipeline);
+	virtual void Process();
+	virtual void Finish();
 
 protected:
 	std::vector<std::string> m_triggerPaths;
-	std::vector<TH1F*> m_triggerResultHists;
+	std::vector<std::string> m_pipelineNames;
+	std::vector<std::vector<TH1F*> > m_triggerEffHists;
 
 };
