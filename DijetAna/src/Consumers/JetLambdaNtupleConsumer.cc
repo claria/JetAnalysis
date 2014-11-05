9a15e121e50b690cd01a9e857de762caa76c2ddd
Georg Sieber <sieber@cern.ch>
2014-11-05 16:01:58 +0100

    more quantities

--- DijetAna/src/Consumers/JetLambdaNtupleConsumer.cc
+++ DijetAna/src/Consumers/JetLambdaNtupleConsumer.cc
@@ -3,6 +3,7 @@
 #include "Artus/Utility/interface/SafeMap.h"
 #include "Artus/Utility/interface/Utility.h"
 #include "Artus/Utility/interface/DefaultValues.h"
+#include "Artus/KappaAnalysis/interface/KappaTypes.h"
 
 #include "JetAnalysis/DijetAna/interface/Consumers/JetLambdaNtupleConsumer.h"
 
@@ -11,28 +12,61 @@
 //	return "JetLambdaNtupleConsumer";
 //}
 
-void JetLambdaNtupleConsumer::Init( setting_type const& settings)
+void JetLambdaNtupleConsumer::Init(setting_type const& settings)
 {
 	// add possible quantities for the lambda ntuples consumers
 	
 	// settings for synch ntuples
-	LambdaNtupleConsumer<JetTypes>::AddQuantity( "evt", [](event_type const& event, product_type const& product)
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("evt", [](KappaEvent const& event, KappaProduct const& product)
 	{
 		return event.m_eventMetadata->nEvent;
 	} );
-	//LambdaNtupleConsumer<JetTypes>::Quantities["puweight"] = [](event_type const& event, product_type const& product) {
-	//	return SafeMap::GetWithDefault(product.m_weights, std::string("puWeight"), 1.0);
-	//};
-	//LambdaNtupleConsumer<JetTypes>::AddQuantity ( "weight", [&settings](event_type const& event, product_type const& product) {
-	//	return SafeMap::GetWithDefault(product.m_weights, settings.GetEventWeight(), 1.0);
-	//} );
-	LambdaNtupleConsumer<JetTypes>::AddQuantity("jet1_pt", LambdaNtupleConsumer<JetTypes>::GetQuantities()["leadingJetPt"] );
-	LambdaNtupleConsumer<JetTypes>::AddQuantity("jet1_eta", LambdaNtupleConsumer<JetTypes>::GetQuantities()["leadingJetEta"] );
-	LambdaNtupleConsumer<JetTypes>::AddQuantity("jet1_phi", LambdaNtupleConsumer<JetTypes>::GetQuantities()["leadingJetPhi"] );
-	LambdaNtupleConsumer<JetTypes>::AddQuantity("jet2_pt", LambdaNtupleConsumer<JetTypes>::GetQuantities()["trailingJetPt"] );
-	LambdaNtupleConsumer<JetTypes>::AddQuantity("jet2_eta", LambdaNtupleConsumer<JetTypes>::GetQuantities()["trailingJetEta"] );
-	LambdaNtupleConsumer<JetTypes>::AddQuantity("jet2_phi", LambdaNtupleConsumer<JetTypes>::GetQuantities()["trailingJetPhi"] );
+	//
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("puweight", [](KappaEvent const& event, KappaProduct const& product) {
+		return SafeMap::GetWithDefault(product.m_weights, std::string("puWeight"), 1.0);
+	});
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("trigweight", [](KappaEvent const& event, KappaProduct const& product) {
+		return SafeMap::GetWithDefault(product.m_weights, std::string("hltPrescaleWeight"), 1.0);
+	});
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("weight", [settings](KappaEvent const& event, KappaProduct const& product) {
+		return SafeMap::GetWithDefault(product.m_weights, settings.GetEventWeight(), 1.0);
+	});
+
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("pathindex",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_selectedHltPosition;
+	} );
+
+
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("njets",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.size();
+	} );
+	// First jet
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet1_pt",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(0)->p4.Pt();
+	} );
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet1_eta",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(0)->p4.Eta();
+	} );
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet1_rap",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(0)->p4.Rapidity();
+	} );
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet1_phi",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(0)->p4.Phi();
+	} );
+	// Second jet
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet2_pt",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(1)->p4.Pt();
+	} );
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet2_eta",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(1)->p4.Eta();
+	} );
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet2_rap",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(1)->p4.Rapidity();
+	} );
+	LambdaNtupleConsumer<KappaTypes>::AddQuantity("jet2_phi",[](KappaEvent const& event, KappaProduct const& product) {
+		return product.m_validJets.at(1)->p4.Phi();
+	} );
 
 	// need to be called at last
-	KappaLambdaNtupleConsumer<JetTypes>::Init(settings);
+	KappaLambdaNtupleConsumer::Init(settings);
 }
