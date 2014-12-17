353cb48a6b29d354c2995596320575fac20f3a1f
Georg Sieber <sieber@cern.ch>
2014-12-17 15:25:21 +0100

    inclusive jet quantities

---
+++ DijetAna/interface/Filters/DijetsRapFilter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Artus/Core/interface/Cpp11Support.h"
+#include "Artus/Core/interface/FilterBase.h"
+
+#include "../JetTypes.h"
+
+class DijetsRapFilter: public JetFilterBase {
+
+	public:
+		virtual std::string GetFilterId() const {
+			return "DijetsRapFilter";
+		}
+
+		virtual void Init(JetSettings const& settings) override;
+		virtual bool DoesEventPass(JetEvent const& event, 
+				JetProduct const& product, JetSettings const& settings) const;
+
+	private:
+		double minDijetsAbsRap;
+		double maxDijetsAbsRap;
+};
+
---
+++ DijetAna/interface/Producers/JetValidJetsProducer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Artus/KappaAnalysis/interface/Producers/ValidJetsProducer.h"
+#include "Artus/KappaAnalysis/interface/KappaTypes.h"
+
+/**
+   \brief Producer for valid jets (simple PF jets).
+   
+*/
+class JetValidJetsProducer: public ValidJetsProducer
+{
+private:
+	float minValidJetPt;
+	float minValidJetAbsRap;
+	float maxValidJetAbsRap;
+protected:
+	virtual bool AdditionalCriteria(KDataPFJet* jet, KappaEvent const& event, KappaProduct & product, KappaSettings const& settings) const override;
+	virtual void Init(KappaSettings const& settings) override;
+};
