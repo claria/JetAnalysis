20b57532cd0abfab6f74a35a82b2f070c58e0f54
Georg Sieber <sieber@cern.ch>
2014-12-17 15:26:41 +0100

    more filters

---
+++ DijetAna/src/Filters/DijetsRapFilter.cc
@@ -0,0 +1,19 @@
+#include "JetAnalysis/DijetAna/interface/Filters/DijetsRapFilter.h"
+
+void DijetsRapFilter::Init(JetSettings const& settings) {
+	JetFilterBase::Init(settings);
+	minDijetsAbsRap = settings.GetMinDijetsAbsRap();
+	maxDijetsAbsRap = settings.GetMaxDijetsAbsRap();
+}
+
+bool DijetsRapFilter::DoesEventPass(JetEvent const& event, JetProduct const& product, JetSettings const& settings) const {
+	bool pass = false;
+	// std::cout << "minmaxcuts " << minDijetsAbsRap << " " << maxDijetsAbsRap << std::endl;
+	if ((std::abs(product.m_validJets.at(0)->p4.Rapidity()) >= minDijetsAbsRap) &&
+	    (std::abs(product.m_validJets.at(0)->p4.Rapidity()) <  maxDijetsAbsRap) &&
+	    (std::abs(product.m_validJets.at(1)->p4.Rapidity()) >= minDijetsAbsRap) &&
+	    (std::abs(product.m_validJets.at(1)->p4.Rapidity()) <  maxDijetsAbsRap)) {
+		pass = true;
+	}
+	return pass;
+}
