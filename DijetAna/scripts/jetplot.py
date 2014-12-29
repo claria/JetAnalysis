ef94a02edaba93723ca1b5e3c2c411a838f6c910
Georg Sieber <sieber@cern.ch>
2014-12-29 20:34:06 +0100

    fix default value bug

--- DijetAna/scripts/jetplot.py
+++ DijetAna/scripts/jetplot.py
@@ -12,7 +12,8 @@ from Artus.HarryPlotter.core import HarryCore
 def harryplotter(args_from_script=None):
 	harry_core = HarryCore(args_from_script=args_from_script)
 	harry_core.register_modules_dir("$CMSSW_BASE/python/JetAnalysis/DijetAna/plotting")
-	harry_core.args['plot_modules'] = ["JetPlot"]
+	if harry_core.args['plot_modules'] == harry_core.parser.get_default('plot_modules'):
+		harry_core.args['plot_modules'] = ["JetPlot"]
 	harry_core.run()
 
 if __name__ == '__main__':
