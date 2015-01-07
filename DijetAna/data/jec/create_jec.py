185c5334c299435307c3383d6ff3e873b83a8ecf
Georg Sieber <sieber@cern.ch>
2015-01-07 10:09:25 +0100

    simplify script a bit

--- DijetAna/data/jec/create_jec.py
+++ DijetAna/data/jec/create_jec.py
@@ -1,10 +1,12 @@
 import FWCore.ParameterSet.Config as cms
+
+gt = 'FT53_V21A_AN6'
+
 process = cms.Process("jectxt")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 # define your favorite global tag
-process.GlobalTag.globaltag = 'FT53_V21A_AN6::All'
-# process.GlobalTag.globaltag = 'START53_V26::All'
+process.GlobalTag.globaltag = '{}::All'.format(gt)
 
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 process.source = cms.Source("EmptySource")
@@ -13,10 +15,28 @@ process.readAK7PF    = cms.EDAnalyzer('JetCorrectorDBReader',
       payloadName    = cms.untracked.string('AK7PF'),
       # this is used ONLY for the name of the printed txt files. You can use any name that you like, 
       # but it is recommended to use the GT name that you retrieved the files from.
-      globalTag      = cms.untracked.string('FT53_V21A_AN6'),  
+      globalTag      = cms.untracked.string(gt),  
       printScreen    = cms.untracked.bool(False),
       createTextFile = cms.untracked.bool(True)
 )
+process.readAK5PF    = cms.EDAnalyzer('JetCorrectorDBReader',  
+      # below is the communication to the database 
+      payloadName    = cms.untracked.string('AK5PF'),
+      # this is used ONLY for the name of the printed txt files. You can use any name that you like, 
+      # but it is recommended to use the GT name that you retrieved the files from.
+      globalTag      = cms.untracked.string(gt),  
+      printScreen    = cms.untracked.bool(False),
+      createTextFile = cms.untracked.bool(True)
+)
+process.readAK5Calo = process.readAK5PF.clone(payloadName = 'AK5Calo')
+process.readAK5JPT = process.readAK5PF.clone(payloadName = 'AK5JPT')
+
 process.readAK7Calo = process.readAK7PF.clone(payloadName = 'AK7Calo')
 process.readAK7JPT = process.readAK7PF.clone(payloadName = 'AK7JPT')
-process.p = cms.Path(process.readAK7PF * process.readAK7Calo * process.readAK7JPT)
+
+process.p = cms.Path(process.readAK5PF*
+                     process.readAK5JPT*
+                     process.readAK5Calo*
+                     process.readAK7PF*
+                     process.readAK7Calo*
+                     process.readAK7JPT)
