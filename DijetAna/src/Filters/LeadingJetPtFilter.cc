345ea472059f85beed8634029f2baf249841ea3f
Georg Sieber <sieber@cern.ch>
2015-02-20 13:13:49 +0100

    simplified code

--- DijetAna/src/Filters/LeadingJetPtFilter.cc
+++ DijetAna/src/Filters/LeadingJetPtFilter.cc
@@ -5,12 +5,11 @@ void LeadingJetPtFilter::Init(JetSettings const& settings) {
 	maxLeadingJetPt = settings.GetMaxLeadingJetPt();
 }
 
-bool LeadingJetPtFilter::DoesEventPass(JetEvent const& event, 
-		JetProduct const& product, JetSettings const& settings) const {
-	bool pass = false;
-	if ((std::abs(product.m_validJets.at(0)->p4.Pt()) >= minLeadingJetPt) &&
-		 std::abs(product.m_validJets.at(0)->p4.Pt()) < maxLeadingJetPt) {
-		pass = true;
+bool LeadingJetPtFilter::DoesEventPass(JetEvent const& event, JetProduct const& product, JetSettings const& settings) const {
+	if ((product.m_validJets.at(0)->p4.Pt() >= minLeadingJetPt) &&
+	     product.m_validJets.at(0)->p4.Pt() < maxLeadingJetPt)
+	{
+		return true;
 	}
-	return pass;
+	return false;
 }
