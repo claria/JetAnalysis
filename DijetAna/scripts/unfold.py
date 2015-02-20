de67112092d494ad4e232e1f06baeb2dcf260dbc
Georg Sieber <sieber@cern.ch>
2015-02-20 13:14:53 +0100

    rudimentary script to perform unfolding

--- DijetAna/scripts/unfold.py
+++ DijetAna/scripts/unfold.py
@@ -1,27 +1,43 @@
 #! /usr/bin/env python2
 
+import argparse
+
 import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
 from ROOT import TFile
 
 def main():
 
+    ROOT.gSystem.Load('/afs/desy.de/user/g/gsieber/dijetana/ana/CMSSW_7_2_3/src/RooUnfold/libRooUnfold.so')
 
-	ROOT.gSystem.Load('/afs/desy.de/user/g/gsieber/dijetana/ana/CMSSW_7_2_3/src/RooUnfold/libRooUnfold.so')
+    parser = argparse.ArgumentParser(description='Unfold distribution with a given response matrix')
+    parser.add_argument('--inputfile', help='Path to root file with the distribution')
+    parser.add_argument('--inputhisto', help='Path to histo in rootfile')
 
-	datafile = TFile('~/dust/testDATA.root', 'UPDATE')
-	responsefile = TFile('/nfs/dust/cms/user/gsieber/ARTUS/2015-02-11_10-23_analysis/output/response_p8.root')
+    parser.add_argument('--responsefile', help='Path to root file with the response matrix')
+    parser.add_argument('--responsehisto', help='Path to response matrix in rootfile')
 
-	hist = datafile.Get('default/h_jet12rap')
-	response = responsefile.Get('default/response_matrix')
-
-	unfold = ROOT.RooUnfoldBayes(response, hist, 4)
-	hReco = unfold.Hreco()
-
-	datafile.cd()
-	hReco.Write('unf_hjet12rap')
+    args = vars(parser.parse_args())
 
 
+    datafile = TFile(args['inputfile'], 'UPDATE')
+    responsefile = TFile(args['responsefile'])
+
+    hist = datafile.Get(args['inputhisto'])
+    response = responsefile.Get(args['responsehisto'])
+
+    # hist = datafile.Get('default/h_jet12rap')
+    # response = responsefile.Get('default/response_matrix')
+
+    unfold = ROOT.RooUnfoldBayes(response, hist, 4)
+    hReco = unfold.Hreco()
+
+    outputfile = TFile('unfolded.root', 'RECREATE')
+    outputfile.mkdir('default')
+    outputfile.cd('default')
+    # datafile.cd()
+    hReco.Write('unf_hjet12rap')
 
 
 if __name__ == '__main__':
-	main()
+    main()
