bccb557e8c71d41f1d97fc9325780895af8d9760
Georg Sieber <g.sieber@gmail.com>
2015-03-02 17:03:49 +0100

    use matched gen jet instead of leading gen jet

--- DijetAna/src/Consumers/JetGenQuantitiesHistogramConsumer.cc
+++ DijetAna/src/Consumers/JetGenQuantitiesHistogramConsumer.cc
@@ -14,7 +14,9 @@ void JetGenQuantitiesHistogramConsumer::Init(setting_type const& settings)
 void JetGenQuantitiesHistogramConsumer::ProcessFilteredEvent(event_type const& event, product_type const& product, setting_type const& settings)
 {
 	double eventWeight = product.m_weights.find(settings.GetEventWeight())->second;
-	m_h2GenVsRecoPt->Fill(product.m_validJets.at(0)->p4.Pt()/event.m_genJets->at(0).p4.Pt(),event.m_genJets->at(0).p4.Pt(), eventWeight);
+	// KLV m_matchedGenJet = *product.m_matchedGenJets.at(0);
+	// std::cout << "Reco jet pt: " << product.m_validJets.at(0)->p4.Pt() << " matched genjet pt " << m_matchedGenJet.p4.Pt() << std::endl;
+	m_h2GenVsRecoPt->Fill(product.m_validJets.at(0)->p4.Pt()/(*product.m_matchedGenJets.at(0)).p4.Pt(), (*product.m_matchedGenJets.at(0)).p4.Pt(), eventWeight);
 }
 
 void JetGenQuantitiesHistogramConsumer::Finish(setting_type const& settings)
