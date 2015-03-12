3bff55556ffc008460239faa188aa78cdb98d332
Georg Sieber <sieber@cern.ch>
2015-03-12 12:30:57 +0100

    use matched genjet

--- DijetAna/src/Consumers/JetUnfoldingResponseConsumer.cc
+++ DijetAna/src/Consumers/JetUnfoldingResponseConsumer.cc
@@ -23,7 +23,7 @@ void JetUnfoldingResponseConsumer::ProcessEvent(event_type const& event, product
 
 	if (result.HasPassed()) 
 	{
-		m_unfoldResponse->Fill(product.m_validJets.at(0)->p4.Rapidity(), product.m_validJets.at(1)->p4.Rapidity(),product.m_validJets.at(0)->p4.Pt(), event.m_genJets->at(0).p4.Rapidity(), event.m_genJets->at(1).p4.Rapidity(), event.m_genJets->at(0).p4.Pt(), eventWeight);
+		m_unfoldResponse->Fill(product.m_validJets.at(0)->p4.Rapidity(), product.m_validJets.at(1)->p4.Rapidity(),product.m_validJets.at(0)->p4.Pt(), (*product.m_matchedGenJets.at(0)).p4.Rapidity(), (*product.m_matchedGenJets.at(1)).p4.Rapidity(), (*product.m_matchedGenJets.at(0)).p4.Pt(), eventWeight);
 	}
 	// TODO Implement correctly the missed events
 	// else 
