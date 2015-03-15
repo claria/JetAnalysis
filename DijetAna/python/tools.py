185989f273f12296c216fe734b22115e98ca2183
Georg Sieber <sieber@cern.ch>
2015-03-15 13:12:40 +0100

    small collection of tools for script usage

---
+++ DijetAna/python/tools.py
@@ -0,0 +1,33 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+ROOT.gROOT.SetBatch(True)
+
+
+def get_root_object(filename, objectpath, option="READ"):
+    rootfile = get_root_file(filename, option=option)
+    obj = rootfile.Get(objectpath)
+    ROOT.SetOwnership(obj, 0)
+    if obj == None:
+        raise Exception("Requested object {0} not found in rootfile {1}.".format(objectpath, filename))
+    return obj
+
+
+def get_root_file(filename, option="READ"):
+    rootfile = ROOT.TFile(filename, option)
+    ROOT.SetOwnership(rootfile, 0)
+    return rootfile
+
+
+def normalize_to(histo, factor=1.0):
+    histo.Sumw2()
+    histo.Scale(1.0 / histo.Integral())
+
+
+def normalize_to_histo(histo, ref_histo):
+    histo.Sumw2()
+    histo.Scale(ref_histo.Integral() / histo.Integral())
+
+
+def normalize_to_binwidth(histo):
+    histo.Sumw2()
+    histo.Scale(1.0, "width")
