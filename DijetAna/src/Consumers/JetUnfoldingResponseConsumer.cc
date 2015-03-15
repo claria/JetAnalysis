c191130ab2b7837a7c1d50486932f05dec2af0e1
Georg Sieber <sieber@cern.ch>
2015-03-15 13:10:21 +0100

    possibility to use separate gen binning

--- DijetAna/src/Consumers/JetUnfoldingResponseConsumer.cc
+++ DijetAna/src/Consumers/JetUnfoldingResponseConsumer.cc
@@ -5,15 +5,13 @@ void JetUnfoldingResponseConsumer::Init(setting_type const& settings)
 {
 	ConsumerBase<JetTypes>::Init(settings);
 
-	// double pt_binning[7] = {74, 114, 196, 300, 468, 790, 3000};
-	// double rap_binning[13] = {-3.0, -2.5, -2.0, -1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0};
-	double* pt_binning = &settings.GetTripleDiffPtBinning()[0];
-	double* rap_binning = &settings.GetRapidityBinning()[0];
-	// std::cout << "ptsize " << settings.GetTripleDiffPtBinning().size() << std::endl;
-	// std::cout << "rapsize " << settings.GetRapidityBinning().size() << std::endl;
-	TH3D meas_binning("meas_binning", "meas_binning", settings.GetRapidityBinning().size() - 1, rap_binning,
-	                                                  settings.GetRapidityBinning().size() - 1, rap_binning, 
-	                                                  settings.GetTripleDiffPtBinning().size() -1, pt_binning);
+	// Define truth and reco binnings for response matrix
+	TH3D meas_binning("meas_binning", "meas_binning", settings.GetRapidityBinning().size() - 1, &settings.GetRapidityBinning()[0],
+	                                                  settings.GetRapidityBinning().size() - 1, &settings.GetRapidityBinning()[0], 
+	                                                  settings.GetTripleDiffPtBinning().size() -1, &settings.GetTripleDiffPtBinning()[0]);
+	// TH3D truth_binning("gen_binning", "gen_binning", settings.GetRapidityBinning().size() - 1, &settings.GetRapidityBinning()[0],
+	//                                                settings.GetRapidityBinning().size() - 1, &settings.GetRapidityBinning()[0], 
+	//                                                settings.GetTripleDiffGenPtBinning().size() -1, &settings.GetTripleDiffGenPtBinning()[0]);
 	m_unfoldResponse = new RooUnfoldResponse(&meas_binning, &meas_binning, "response_matrix", "response_matrix");
 }
 
@@ -23,13 +21,22 @@ void JetUnfoldingResponseConsumer::ProcessEvent(event_type const& event, product
 
 	if (result.HasPassed()) 
 	{
-		m_unfoldResponse->Fill(product.m_validJets.at(0)->p4.Rapidity(), product.m_validJets.at(1)->p4.Rapidity(),product.m_validJets.at(0)->p4.Pt(), (*product.m_matchedGenJets.at(0)).p4.Rapidity(), (*product.m_matchedGenJets.at(1)).p4.Rapidity(), (*product.m_matchedGenJets.at(0)).p4.Pt(), eventWeight);
+		if (product.m_matchedRecoJets.at(0) != NULL && product.m_matchedRecoJets.at(1) != NULL) {
+			m_unfoldResponse->Fill(product.m_matchedRecoJets.at(0)->p4.Rapidity(), 
+					               product.m_matchedRecoJets.at(1)->p4.Rapidity(),
+					               product.m_matchedRecoJets.at(0)->p4.Pt(), 
+					               event.m_genJets->at(0).p4.Rapidity(), 
+					               event.m_genJets->at(1).p4.Rapidity(), 
+					               event.m_genJets->at(0).p4.Pt(), 
+					               eventWeight);
+		}
+		else {
+			m_unfoldResponse->Miss(event.m_genJets->at(0).p4.Rapidity(), 
+					               event.m_genJets->at(1).p4.Rapidity(), 
+					               event.m_genJets->at(0).p4.Pt(), 
+					               eventWeight);
+		}
 	}
-	// TODO Implement correctly the missed events
-	// else 
-	// {
-		// m_unfoldResponse->Miss(event.m_genJets->at(0).p4.Pt(), event.m_genJets->at(0).p4.Rapidity(), eventWeight);
-	// }
 }
 
 void JetUnfoldingResponseConsumer::Finish(setting_type const& settings)
