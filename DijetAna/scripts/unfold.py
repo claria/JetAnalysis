0974dd26a9483cb12ab364aac210752bf6d98777
Georg Sieber <sieber@cern.ch>
2015-03-18 22:58:43 +0100

    comment out cov/corr part for now

--- DijetAna/scripts/unfold.py
+++ DijetAna/scripts/unfold.py
@@ -6,59 +6,53 @@ import argparse
 
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
-from ROOT import TFile
+from JetAnalysis.DijetAna.tools import *
 
 def main():
 
     ROOT.gSystem.Load('/afs/desy.de/user/g/gsieber/dijetana/ana/CMSSW_7_2_3/src/RooUnfold/libRooUnfold.so')
 
     parser = argparse.ArgumentParser(description='Unfold distribution with a given response matrix')
-    parser.add_argument('--inputfile', help='Path to root file with the distribution')
-    parser.add_argument('--inputhisto', help='Path to histo in rootfile')
 
-    parser.add_argument('--responsefile', help='Path to root file with the response matrix')
-    parser.add_argument('--responsehisto', help='Path to response matrix in rootfile')
+    parser.add_argument('--recohisto', help='Path to root file with the distribution')
+    parser.add_argument('--response', help='Path to root file with the response matrix')
+    parser.add_argument('--niters', help='Number of iterations in unfolding' type=int)
 
     args = vars(parser.parse_args())
 
 
-    datafile = TFile(args['inputfile'], 'UPDATE')
-    responsefile = TFile(args['responsefile'])
-
-    hist = datafile.Get(args['inputhisto'])
-    response = responsefile.Get(args['responsehisto'])
-
-    # hist = datafile.Get('default/h_jet12rap')
-    # response = responsefile.Get('default/response_matrix')
+    hist = get_root_object(args['inputhisto'], option='UPDATE')
+    response = get_root_object(args['responsehisto'])
 
     unfold = ROOT.RooUnfoldBayes(response, hist, 4)
     print unfold.NToys()
-    unfold.SetNToys(1000)
+    unfold.SetNToys(2)
     print unfold.NToys()
     unfold.RunToy()
     hReco = unfold.Hreco()
     hRecoCov = unfold.Ereco(3)
 
-    hRecoCorr = hRecoCov.Clone('hrecocorr')
-    print type(hRecoCorr)
-    print dir(hRecoCorr)
-    print hRecoCorr.GetNrows()
+    #hRecoCorr = hRecoCov.Clone('hrecocorr')
+    #print type(hRecoCorr)
+    #print dir(hRecoCorr)
+    #print hRecoCorr.GetNrows()
 
-    for i in range(0, hRecoCorr.GetNrows()):
-        for j in range(i,hRecoCorr.GetNrows()):
-            tmp = (hRecoCov[i][i]*hRecoCov[j][j])
-            if tmp <= 0.:
-                hRecoCorr[i][j] = 0.
-            else:
-                hRecoCorr[i][j] = (hRecoCov[i][j] / (hRecoCov[i][i]*hRecoCov[j][j]))
-                if hRecoCorr[i][j] > 1.:
-                    print i, j, hRecoCorr[i][j]
-                hRecoCorr[i][j] = 0.0
-            hRecoCorr[j][i] = hRecoCorr[i][j]
+    #for i in range(0, hRecoCorr.GetNrows()):
+    #    for j in range(i,hRecoCorr.GetNrows()):
+    #        tmp = (hRecoCov[i][i]*hRecoCov[j][j])
+    #        if tmp <= 0.:
+    #            hRecoCorr[i][j] = 0.
+    #        else:
+    #            hRecoCorr[i][j] = (hRecoCov[i][j] / (hRecoCov[i][i]*hRecoCov[j][j]))
+    #            if hRecoCorr[i][j] > 1.:
+    #                print i, j, hRecoCorr[i][j]
+    #            hRecoCorr[i][j] = 0.0
+    #        hRecoCorr[j][i] = hRecoCorr[i][j]
 
     # for i in range 
 
     # outputfile = TFile('unfolded.root', 'RECREATE')
+
     datafile.cd('/')
     if datafile.GetDirectory('unf_default') != None:
         datafile.Delete('unf_default;*')
