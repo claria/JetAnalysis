13a15d47afd5955b9d42ae1ff7903923c75e1ec1
Georg Sieber <sieber@cern.ch>
2015-04-02 15:35:30 +0200

    write out log into file

--- DijetAna/data/run_jetana.sh
+++ DijetAna/data/run_jetana.sh
@@ -6,7 +6,6 @@ echo "---------------------"
 echo "Prepare to run JetAna"
 echo "---------------------"
 
-
 echo "-------------------------"
 echo "Listing current directory"
 echo "-------------------------"
@@ -19,6 +18,6 @@ echo "---------------------------------"
 echo "Running with the following config"
 echo "---------------------------------"
 # $FILE_NAMES=echo "${FILE_NAMES//\"}"
-dijetana.py -i ${FILE_NAMES//\"} --save-config config.json -c $CONFIG --log-level debug || exit $?
+dijetana.py -i ${FILE_NAMES//\"} --save-config config.json -c $CONFIG --log-level debug > out.log 2>&1 || exit $?
 
 exit 0
