4e804aed4e3ba1e05f46e3958437f01c6069afea
Georg Sieber <sieber@cern.ch>
2015-06-19 17:25:42 +0200

    move binning to baseconfig.

--- DijetAna/python/artusconfigs/runconfig.py
+++ DijetAna/python/artusconfigs/runconfig.py
@@ -17,9 +17,6 @@ class RunConfig(BaseConfig):
         self['GenPtBinning'] = [74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
         self['TripleDiffGenPtBinning'] = [74, 114, 196, 300, 468, 790, 3000]
         # Binnings
-        self['RapidityAbsBinning'] = [0.0, 1.0, 2.0, 3.0]
-        self['RapidityBinning'] = [-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0]
-
         default_pipeline = self.get_default_pipeline()
         default_pipeline['Consumers'] =  [
                                   'KappaLambdaNtupleConsumer',
