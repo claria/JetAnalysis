6fb09157b25c4aea882d686bd080d43630fcaceb
Georg Sieber <sieber@cern.ch>
2015-06-29 16:30:39 +0200

    config for gen only studies

---
+++ DijetAna/python/artusconfigs/genconfig.py
@@ -0,0 +1,44 @@
+from baseconfig import BaseConfig
+import copy
+
+
+class GenConfig(BaseConfig):
+
+    def __init__(self, nick=None, config=None):
+        super(GenConfig, self).__init__(nick=nick, config=config)
+
+        if self.is_data is True:
+            raise Exception("This config only works on GEN")
+
+    def modify_settings(self):
+        self['GenLumiMetadata'] = 'lumiInfo'
+        self['GenEventMetadata'] = 'eventInfo'
+        self['GenJets'] = 'ak7GenJets'
+        self['TrackSummary'] = ''
+        self['VertexSummary'] = ''
+        self['BasicJets'] = ''
+        self['PileupDensity'] = ''
+        self['Met'] = ''
+
+
+        self['MinValidGenJetPt'] = 15.
+        self['MaxDeltaR'] = 0.3
+        self['PtBinning'] = [50, 56, 64, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
+        self['Processors'] = [
+            'producer:ValidGenJetsProducer',
+            'producer:GenJetQuantitiesProducer',
+            'producer:CrossSectionWeightProducer',
+            'producer:GeneratorWeightProducer',
+            'producer:NumberGeneratedEventsWeightProducer',
+            'producer:EventWeightProducer'
+
+        ]
+
+        default_pipeline = self.get_default_pipeline()
+        default_pipeline['Consumers'] =  [
+                                  'GenJetQuantitiesHistogramConsumer',
+                                  ]
+
+
+    def expand_pipelines(self):
+        pass
