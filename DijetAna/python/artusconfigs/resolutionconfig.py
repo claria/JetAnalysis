e4a268624f2eedf6744925d0a6e3818fb70d452a
Georg Sieber <g.sieber@gmail.com>
2015-07-22 15:01:01 +0200

    use new filters.

--- DijetAna/python/artusconfigs/resolutionconfig.py
+++ DijetAna/python/artusconfigs/resolutionconfig.py
@@ -13,7 +13,12 @@ class ResolutionConfig(BaseConfig):
     def modify_settings(self):
         self['MinValidJetPt'] = 15.
         self['MaxDeltaR'] = 0.3
-        self['PtBinning'] = [50, 56, 64, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
+        self['PtBinning'] = [34, 42, 50, 58, 66, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
+
+        self['MinValidGenJetPt'] = 00.
+        self['MinValidJetPt'] = 00.
+        self['MinLeadingGenJetPt'] = 0.0
+        self['MinLeadingJetPt'] = 0.0
         default_pipeline = self.get_default_pipeline()
         default_pipeline['Consumers'] =  [
                                   'cutflow_histogram',
@@ -23,15 +28,18 @@ class ResolutionConfig(BaseConfig):
 
     def expand_pipelines(self):
         pass
-        # rap_bins = [(0.0,0.5),(0.5,1.0),(1.0,1.5),(1.5,2.0),(2.0,2.5),(2.5,3.0)]
-        rap_bins = [(0.0,1.0),(1.0,2.0),(2.0,3.0)]
-        for yboost_bin in rap_bins:
-            for ystar_bin in rap_bins:
-                pipeline_name = 'ystar_{0}_{1}_yboost_{2}_{3}'.format(ystar_bin[0],ystar_bin[1],yboost_bin[0],yboost_bin[1]).replace('.','')
+        for i in range(0, len(self['RapidityAbsBinning']) -1):
+            for j in range(0, len(self['RapidityAbsBinning']) -1):
+                yb_lo = self['RapidityAbsBinning'][i]
+                yb_hi = self['RapidityAbsBinning'][i+1]
+                ys_lo = self['RapidityAbsBinning'][j]
+                ys_hi = self['RapidityAbsBinning'][j+1]
+                # reco pipelines
+                pipeline_name = 'ptavg_yb_{0}_{1}_ys_{2}_{3}'.format(yb_lo, yb_hi, ys_lo, ys_hi).replace('.','')
                 self['Pipelines'][pipeline_name] = copy.deepcopy(self['Pipelines']['default'])
                 self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:YStarFilter')
                 self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:YBoostFilter')
-                self['Pipelines'][pipeline_name]['MinYStar'] = ystar_bin[0]
-                self['Pipelines'][pipeline_name]['MaxYStar'] = ystar_bin[1]
-                self['Pipelines'][pipeline_name]['MinYBoost'] = yboost_bin[0]
-                self['Pipelines'][pipeline_name]['MaxYBoost'] = yboost_bin[1]
+                self['Pipelines'][pipeline_name]['MinYStar'] = ys_lo
+                self['Pipelines'][pipeline_name]['MaxYStar'] = ys_hi
+                self['Pipelines'][pipeline_name]['MinYBoost'] = yb_lo
+                self['Pipelines'][pipeline_name]['MaxYBoost'] = yb_hi
