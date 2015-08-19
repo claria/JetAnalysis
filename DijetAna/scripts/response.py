0e10de49ef95c78ba073d82a2e301aca949d559c
Georg Sieber <g.sieber@gmail.com>
2015-08-19 09:49:21 +0200

    fill all response matrices.

--- DijetAna/scripts/response.py
+++ DijetAna/scripts/response.py
@@ -22,15 +22,15 @@ def smear_pt(rap_bin, pt_truth):
         return np.sqrt((N/x)**2 + (S**2)/x + C**2)
 
     res_pars = {
-            'ptavg_yb_00_10_ys_00_10' : [4.55108862204, 0.664407443168, 0.0283713960455, 0.0],
-            'ptavg_yb_00_10_ys_10_20' : [6.54217125781, 0.369798623098, 0.0350309905636, 0.0],
-            'ptavg_yb_00_10_ys_20_30' : [4.7553191831, 0.623381451417, 5.86709966666e-07, 0.0],
-            'ptavg_yb_10_20_ys_00_10' : [6.30077879419, 0.402135274691, 0.0345126768626, 0.0],
-            'ptavg_yb_10_20_ys_10_20' : [4.5465663911, 0.639092518774, 0.0206529461783, 0.0],
-            'ptavg_yb_10_20_ys_20_30' : [-2828.50171988, 0.5, -7.33267613295e-15, 0.0],
-            'ptavg_yb_20_30_ys_00_10' : [5.11347922038, 0.602555697342, 7.9639541027e-07, 0.0],
-            'ptavg_yb_20_30_ys_10_20' : [-2828.50171988, 0.5, -7.33267613295e-15, 0.0],
-            'ptavg_yb_20_30_ys_20_30' : [-2828.50171988, 0.5, -7.33267613295e-15, 0.0],
+            'ptavg_yb_00_10_ys_00_10' : [5.22275e+00, 9.60557e-01, 3.49387e-02], 
+            'ptavg_yb_00_10_ys_10_20' : [6.21719e+00, 8.38538e-01, 3.69017e-02],
+            'ptavg_yb_00_10_ys_20_30' : [6.05380e+00, 8.59112e-01, -5.60583e-07],
+            'ptavg_yb_10_20_ys_00_10' : [6.06379e+00, 8.57709e-01, -3.49501e-02],
+            'ptavg_yb_10_20_ys_10_20' : [5.30264e+00, 9.35802e-01, -2.40824e-02],
+            # 'ptavg_yb_10_20_ys_20_30' : [-2828.50171988, 0.5, -7.33267613295e-15, 0.0],
+            'ptavg_yb_20_30_ys_00_10' : [5.85437e+00, 8.73750e-01, 1.76659e-08],
+            # 'ptavg_yb_20_30_ys_10_20' : [-2828.50171988, 0.5, -7.33267613295e-15, 0.0],
+            # 'ptavg_yb_20_30_ys_20_30' : [-2828.50171988, 0.5, -7.33267613295e-15, 0.0],
 
             }
     res = nsc_formula(pt_truth, res_pars[rap_bin][0], res_pars[rap_bin][1], res_pars[rap_bin][2])
@@ -43,10 +43,6 @@ def smear_pt(rap_bin, pt_truth):
 def main():
 
     ROOT.gSystem.Load('/nfs/dust/cms/user/gsieber/dijetana/ana/CMSSW_7_2_3/src/RooUnfold/libRooUnfold.so')
-
-    # fnlo = fastNLOLHAPDF('/nfs/dust/cms/user/gsieber/FNLO/TRIPLEDIFF_YSB_PTAVG/triplediff.tab')
-    # fnlo.SetLHAPDFFilename('CT10nlo')
-    # fnlo.SetLHAPDFMember(0)
 
     rap_bins = ['ptavg_yb_00_10_ys_00_10',
                 'ptavg_yb_00_10_ys_10_20',
@@ -63,7 +59,7 @@ def main():
     pt_binning_reco = [34, 42, 50, 58, 66, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
 
     nlofile = ROOT.TFile('/afs/desy.de/user/g/gsieber/XSNLO.root', 'READ')
-    f = ROOT.TFile('unf_fastNLO.root', 'RECREATE')
+    f = ROOT.TFile('response_fastNLO.root', 'RECREATE')
 
     fit_start_params = {
             'ptavg_yb_00_10_ys_00_10' : [1.56951e-04, 4.72895e+00, 1.25370e+01, 4.41885e+03],
@@ -71,14 +67,14 @@ def main():
             'ptavg_yb_00_10_ys_20_30' : [2.71653e+00, 4.28902e+00, 7.67473e+00, 5.83335e+02],
             'ptavg_yb_10_20_ys_00_10' : [4.56147e-04, 4.88011e+00, 1.26727e+01, 2.80141e+03],
             'ptavg_yb_10_20_ys_10_20' : [1.62403e-01, 3.89047e+00, 1.73026e+01, 1.40653e+03],
-            'ptavg_yb_10_20_ys_20_30' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
+            # 'ptavg_yb_10_20_ys_20_30' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
             'ptavg_yb_20_30_ys_00_10' : [2.39237e-02, 4.39508e+00, 1.41838e+01, 1.16487e+03],
-            'ptavg_yb_20_30_ys_10_20' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
-            'ptavg_yb_20_30_ys_20_30' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
+            # 'ptavg_yb_20_30_ys_10_20' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
+            # 'ptavg_yb_20_30_ys_20_30' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
 
             }
 
-    for rap_bin in rap_bins[0:1]:
+    for rap_bin in rap_bins:
         print rap_bin
         xsnlo = nlofile.Get("{0}/xsnlo".format(rap_bin))
         # Fitting spectrum
@@ -90,15 +86,6 @@ def main():
         # res = xsnlo.Fit("nlo_fcn", "RS", "")
         nlo_fcn.SetRange(74., 3000.)
         res = xsnlo.Fit("nlo_fcn", "RSI0", "")
-        # if res.Get() == None or res.Status() != 0:
-        #     nlo_fcn.SetRange(100., 1000.)
-        #     # res = xsnlo.Fit("nlo_fcn", "RSW", "")
-        #     res = xsnlo.Fit("nlo_fcn", "RS", "")
-        #     nlo_fcn.SetRange(74., 2500.)
-        #     res = xsnlo.Fit("nlo_fcn", "RSI", "")
-        #     if res.Get() == None or res.Status() != 0:
-        #         continue
-
 
         #TODO Fit this function
         f.mkdir(rap_bin)
@@ -123,25 +110,35 @@ def main():
 
         n_evts = 10000000
         for i in xrange(n_evts):
-            pt_truth = ROOT.gRandom.Uniform(74.,4000)
+            pt_truth = ROOT.gRandom.Uniform(50.,4000)
             pt_smeared = smear_pt(rap_bin, pt_truth)
             w = nlo_fcn.Eval(pt_truth)
+            if (w< 0.) or math.isnan(w):
+                w = 0.
             # if pt_smeared < 62.:
                 # response_ptavg.Miss(pt_truth, w)
             # else:
-            response_ptavg.Fill(pt_smeared, pt_truth, w)
+            if pt_truth > 62. and pt_smeared > 62.:
+                response_ptavg.Fill(pt_smeared, pt_truth, w)
+            elif pt_truth > 62. and pt_smeared <=62.:
+                response_ptavg.Miss(pt_truth, w)
+            elif pt_truth <= 62. and pt_smeared > 62.:
+                response_ptavg.Fake(pt_truth, w)
 
-            # pt_truth = ROOT.gRandom.Uniform(30.,4000)
-            # pt_smeared = smear_pt(rap_bin, pt_truth)
-            # w = nlo_fcn.Eval(pt_truth)
             h_genptavg.Fill(pt_truth, w)
             h_recoptavg.Fill(pt_smeared, w)
 
-        response_ptavg.Write('response_ptavg')
+        response_ptavg.Write('res_matrix_ptavg')
         # h_recoptavg.Scale(1.0, "width")
         h_recoptavg.Write()
         # h_genptavg.Scale(1.0, "width")
         h_genptavg.Write()
+        del response_ptavg
+        del h_recoptavg
+        del h_genptavg
+
+
+    f.Close()
 
 if __name__ == '__main__':
     main()
