109a6cb73e98f4b30b87cf1ff4cf091734552d57
Georg Sieber <g.sieber@gmail.com>
2015-09-20 20:40:16 +0200

    apparently git stuff is needed to checkout cms packages

--- test_build.sh
+++ test_build.sh
@@ -3,6 +3,11 @@
 set -x
 set -e
 
+
+git config --global user.name 'Georg Sieber'
+git config --global user.email 'sieber@cern.ch'
+git config --global user.github 'claria'
+
 # Mount cvmfs
 /etc/cvmfs/run-cvmfs.sh
 export SCRAM_ARCH=slc6_amd64_gcc481
