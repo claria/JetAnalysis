f68715529dd464850ee3c5b8a8f3e2445273fbbf
Georg Sieber <g.sieber@gmail.com>
2015-10-05 09:18:59 +0200

    revert to nloxnp.

--- DijetAna/scripts/response.py
+++ DijetAna/scripts/response.py
@@ -11,8 +11,36 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 from ROOT import TFile
 from array import array
 import numpy as np
+from JetAnalysis.DijetAna.tools import get_root_object
 
 # from fastnlo import fastNLOLHAPDF
+def multiply(obj, with_obj):
+    obj = obj.Clone('ratio_{0}'.format(obj.GetName()))
+
+    if isinstance(with_obj, ROOT.TH1):
+        mult_vals = np.zeros(obj.GetNbinsX())
+        for i in xrange(1, with_obj.GetNbinsX() + 1):
+            mult_vals[i-1] = with_obj.GetBinContent(i)
+    elif isinstance(with_obj, ROOT.TGraph):
+        mult_vals = np.zeros(with_obj.GetN())
+        for i in xrange(with_obj.GetN()):
+            tmp_x, tmp_y = ROOT.Double(0), ROOT.Double(0)
+            with_obj.GetPoint(i, tmp_x, tmp_y)
+            mult_vals[i] = tmp_y
+
+    if isinstance(obj, ROOT.TH1):
+        for i in xrange(1, obj.GetNbinsX() + 1):
+            obj.SetBinContent(i, obj.GetBinContent(i) * mult_vals[i-1])
+            obj.SetBinError(i, obj.GetBinError(i) * mult_vals[i-1])
+    elif isinstance(obj, ROOT.TGraph):
+        for i in xrange(obj.GetN()):
+            tmp_x, tmp_y = ROOT.Double(0), ROOT.Double(0)
+            obj.GetPoint(i, tmp_x, tmp_y)
+            obj.SetPoint(i, tmp_x, tmp_y * mult_vals[i])
+    else:
+        raise TypeError('Invalid types passed: {0} and {1}'.format(type(obj), type(to_obj)))
+    return obj
+
 
 
 def smear_pt(rap_bin, pt_truth):
@@ -32,7 +60,6 @@ def smear_pt(rap_bin, pt_truth):
     res = nsc_formula(pt_truth, res_pars[rap_bin][0], res_pars[rap_bin][1], res_pars[rap_bin][2], res_pars[rap_bin][3])
 
     shift = ROOT.gRandom.Gaus(0., res*pt_truth)
-    # print shift, pt_truth + shift
 
     return pt_truth + shift
 
@@ -51,22 +78,24 @@ def main():
     pt_binning_gen = [34, 42, 50, 58, 66, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
     pt_binning_reco = [34, 42, 50, 58, 66, 74, 84, 97, 114, 133, 153, 174, 196, 220, 245, 272, 300, 330, 362, 395, 430, 468, 507, 548, 592, 638, 686, 737, 790, 846, 905, 967, 1032, 1101, 1172, 1248, 1327, 1410, 1497, 1588, 1784, 2116, 2500, 3000]
 
-    nlofile = ROOT.TFile('/afs/desy.de/user/g/gsieber/PTAVG_YBYS_NLO.root', 'READ')
     f = ROOT.TFile('response_fastNLO.root', 'RECREATE')
 
     fit_start_params = {
             'yb0ys0' : [1.56951e-04, 4.72895e+00, 1.25370e+01, 4.41885e+03],
-            'yb1ys0' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
-            'yb2ys0' : [2.71653e+00, 4.28902e+00, 7.67473e+00, 5.83335e+02],
-            'yb0ys1' : [4.56147e-04, 4.88011e+00, 1.26727e+01, 2.80141e+03],
+            'yb0ys1' : [1.25998e-02, 4.86297e+00, 7.80386e+00, 1.58397e+03],
+            'yb0ys2' : [2.71653e+00, 4.28902e+00, 7.67473e+00, 5.83335e+02],
+            'yb1ys0' : [4.56147e-04, 4.88011e+00, 1.26727e+01, 2.80141e+03],
             'yb1ys1' : [1.62403e-01, 3.89047e+00, 1.73026e+01, 1.40653e+03],
-            'yb0ys2' : [2.39237e-02, 4.39508e+00, 1.41838e+01, 1.16487e+03],
+            'yb2ys0' : [2.39237e-02, 4.39508e+00, 1.41838e+01, 1.16487e+03],
 
             }
 
     for rap_bin in rap_bins:
         print rap_bin
-        xsnlo = nlofile.Get("{0}/CT10nlo_xs".format(rap_bin))
+        xsnlo = get_root_object("/nfs/dust/cms/user/gsieber/dijetana/ana/CMSSW_7_2_3/PTAVG_YBYS_NLO.root?{0}/CT14nlo_xs".format(rap_bin))
+        np_corr = get_root_object('~/dust/dijetana/plot/plots/np_factors_calc_{0}.root?res_np_factor'.format(rap_bin))
+        f.cd()
+        xsnlo = multiply(xsnlo, np_corr)
         # Fitting spectrum
         ROOT.TVirtualFitter.SetMaxIterations(9999)
         nlo_fcn = ROOT.TF1("nlo_fcn", "[0]*(x/[3])**(-1*[1])*(1-(x/[3]))**[2]")
@@ -74,7 +103,7 @@ def main():
         # nlo_fcn.SetRange(100., 1000.)
         # res = xsnlo.Fit("nlo_fcn", "RSW", "")
         # res = xsnlo.Fit("nlo_fcn", "RS", "")
-        nlo_fcn.SetRange(74., 3000.)
+        nlo_fcn.SetRange(74.,3000.)
         res = xsnlo.Fit("nlo_fcn", "RSI0")
 
         if res.Get() == None or res.Status() != 0:
@@ -106,22 +135,17 @@ def main():
 
         # response_ptavg = ROOT.RooUnfoldResponse(reco_binning_ptavg, gen_binning_ptavg)
 
-        nlo_fcn.SetRange(36., 3000.)
+        nlo_fcn.SetRange(50., 3000.)
 
         n_evts = 10000000
         for i in xrange(n_evts):
-            pt_truth = ROOT.gRandom.Uniform(40.,3000)
+            pt_truth = ROOT.gRandom.Uniform(50.,3000)
             pt_smeared = smear_pt(rap_bin, pt_truth)
             w = nlo_fcn.Eval(pt_truth)
             if (w< 0.) or math.isnan(w):
                 w = 0.
-            # if pt_smeared < 62.:
-                # response_ptavg.Miss(pt_truth, w)
-            # else:
-            if pt_truth > 62.:
-                h_genptavg.Fill(pt_truth, w)
-            if pt_smeared > 62.:
-                h_recoptavg.Fill(pt_smeared, w)
+            h_genptavg.Fill(pt_truth, w)
+            h_recoptavg.Fill(pt_smeared, w)
             h2_genvsreco.Fill(pt_smeared, pt_truth, w)
 
         h_recoptavg.Write()
