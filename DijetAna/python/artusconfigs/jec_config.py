a72496bf21fa289dd149756b77806ce8b253de45
Georg Sieber <g.sieber@gmail.com>
2015-10-13 12:57:00 +0200

    use subset of pipelines.

--- DijetAna/python/artusconfigs/jec_config.py
+++ DijetAna/python/artusconfigs/jec_config.py
@@ -46,30 +46,29 @@ class JECConfig(RunConfig):
                           'Total',
                           ]
 
-        for i in range(0, len(self['RapidityAbsBinning']) -1):
-            for j in range(0, len(self['RapidityAbsBinning']) -1):
-                yb_lo = self['RapidityAbsBinning'][i]
-                yb_hi = self['RapidityAbsBinning'][i+1]
-                ys_lo = self['RapidityAbsBinning'][j]
-                ys_hi = self['RapidityAbsBinning'][j+1]
-                # reco pipelines
-                pipeline_name = 'yb{0}ys{1}'.format(i, j)
-                self['Pipelines'][pipeline_name] = copy.deepcopy(self['Pipelines']['default'])
-                self['Pipelines'][pipeline_name]['Processors'].insert(self['Pipelines'][pipeline_name]['Processors'].index('producer:JetQuantitiesProducer') + 1,'filter:YBoostFilter')
-                self['Pipelines'][pipeline_name]['Processors'].insert(self['Pipelines'][pipeline_name]['Processors'].index('producer:JetQuantitiesProducer') + 1,'filter:YStarFilter')
-                self['Pipelines'][pipeline_name]['MinYStar'] = ys_lo
-                self['Pipelines'][pipeline_name]['MaxYStar'] = ys_hi
-                self['Pipelines'][pipeline_name]['MinYBoost'] = yb_lo
-                self['Pipelines'][pipeline_name]['MaxYBoost'] = yb_hi
+        for i, j in [(0,0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0)]:
+            yb_lo = self['RapidityAbsBinning'][i]
+            yb_hi = self['RapidityAbsBinning'][i+1]
+            ys_lo = self['RapidityAbsBinning'][j]
+            ys_hi = self['RapidityAbsBinning'][j+1]
+            # reco pipelines
+            pipeline_name = 'yb{0}ys{1}'.format(i, j)
+            self['Pipelines'][pipeline_name] = copy.deepcopy(self['Pipelines']['default'])
+            self['Pipelines'][pipeline_name]['Processors'].insert(self['Pipelines'][pipeline_name]['Processors'].index('producer:JetQuantitiesProducer') + 1,'filter:YBoostFilter')
+            self['Pipelines'][pipeline_name]['Processors'].insert(self['Pipelines'][pipeline_name]['Processors'].index('producer:JetQuantitiesProducer') + 1,'filter:YStarFilter')
+            self['Pipelines'][pipeline_name]['MinYStar'] = ys_lo
+            self['Pipelines'][pipeline_name]['MaxYStar'] = ys_hi
+            self['Pipelines'][pipeline_name]['MinYBoost'] = yb_lo
+            self['Pipelines'][pipeline_name]['MaxYBoost'] = yb_hi
 
-                for source in jec_unc_names:
-                    # Upwards 
-                    pipeline_name_up = '{0}_{1}_up'.format(pipeline_name, source)
-                    self['Pipelines'][pipeline_name_up] = copy.deepcopy(self['Pipelines'][pipeline_name])
-                    self['Pipelines'][pipeline_name_up]['JetEnergyCorrectionUncertaintySource'] = source
-                    self['Pipelines'][pipeline_name_up]['JetEnergyCorrectionUncertaintyShift'] = 1.0
-                    # Downwards 
-                    pipeline_name_dn = '{0}_{1}_dn'.format(pipeline_name, source)
-                    self['Pipelines'][pipeline_name_dn] = copy.deepcopy(self['Pipelines'][pipeline_name])
-                    self['Pipelines'][pipeline_name_dn]['JetEnergyCorrectionUncertaintyShift'] = -1.0
+            for source in jec_unc_names:
+                # Upwards 
+                pipeline_name_up = '{0}_{1}_up'.format(pipeline_name, source)
+                self['Pipelines'][pipeline_name_up] = copy.deepcopy(self['Pipelines'][pipeline_name])
+                self['Pipelines'][pipeline_name_up]['JetEnergyCorrectionUncertaintySource'] = source
+                self['Pipelines'][pipeline_name_up]['JetEnergyCorrectionUncertaintyShift'] = 1.0
+                # Downwards 
+                pipeline_name_dn = '{0}_{1}_dn'.format(pipeline_name, source)
+                self['Pipelines'][pipeline_name_dn] = copy.deepcopy(self['Pipelines'][pipeline_name])
+                self['Pipelines'][pipeline_name_dn]['JetEnergyCorrectionUncertaintyShift'] = -1.0
 
