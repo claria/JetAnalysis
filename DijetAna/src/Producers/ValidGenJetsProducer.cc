00fc8ba1923df1a00e36292ee28516d36f4f7650
Georg Sieber <g.sieber@gmail.com>
2015-11-09 14:48:39 +0100

    clean up code

--- DijetAna/src/Producers/ValidGenJetsProducer.cc
+++ DijetAna/src/Producers/ValidGenJetsProducer.cc
@@ -11,23 +11,17 @@ void ValidGenJetsProducer::Init(JetSettings const& settings) {
   maxValidGenJetAbsRap = settings.GetMaxValidGenJetAbsRap();
 }
 void ValidGenJetsProducer::Produce(JetEvent const& event, JetProduct& product, JetSettings const& settings) const {
-  for (auto genjet = event.m_genJets->begin(); genjet != event.m_genJets->end(); genjet++) {
-    if (AdditionalCriteria(&(*genjet), event, product, settings)) {
+  for (auto jet = event.m_genJets->begin(); jet != event.m_genJets->end(); jet++) {
+    bool validJet = true:
+    validJet = validJet
+      && (jet->p4.Pt() > minValidGenJetPt) 
+      && (std::abs(jet->p4.Rapidity()) >= minValidGenJetAbsRap) 
+      && (std::abs(jet->p4.Rapidity()) < maxValidGenJetAbsRap)
+    if (validJet) 
+    {
       product.m_validGenJets.push_back(&(*genjet));
     }
   }
-  // for (auto i = product.m_validGenJets.begin(); i != product.m_validGenJets.end(); ++i)
-  //   std::cout << (*i)->p4.Pt() << ' ';
-  // std::cout <<std::endl;
 }
 
-bool ValidGenJetsProducer::AdditionalCriteria(KLV* jet,
-                                              JetEvent const& event,
-                                              JetProduct& product,
-                                              JetSettings const& settings) const {
-  if ((jet->p4.Pt() > minValidGenJetPt) && (std::abs(jet->p4.Rapidity()) >= minValidGenJetAbsRap) &&
-      (std::abs(jet->p4.Rapidity()) < maxValidGenJetAbsRap)) {
-    return true;
-  }
-  return false;
-}
+
