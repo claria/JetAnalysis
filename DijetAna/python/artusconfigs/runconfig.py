ee324bf0475876d00eba08db03e664d2f00b5f18
Georg Sieber <g.sieber@gmail.com>
2015-11-17 11:32:25 +0100

    updated runconfig.

--- DijetAna/python/artusconfigs/runconfig.py
+++ DijetAna/python/artusconfigs/runconfig.py
@@ -99,31 +99,31 @@ class RunConfig(BaseConfig):
  
     def expand_pipelines(self):
 
-        for i in range(0, len(self['RapidityAbsBinning']) -1):
-            for j in range(0, len(self['RapidityAbsBinning']) -1):
-                yb_lo = self['RapidityAbsBinning'][i]
-                yb_hi = self['RapidityAbsBinning'][i+1]
-                ys_lo = self['RapidityAbsBinning'][j]
-                ys_hi = self['RapidityAbsBinning'][j+1]
-                # reco pipelines
-                pipeline_name = 'yb{0}ys{1}'.format(i, j)
-                self['Pipelines'][pipeline_name] = copy.deepcopy(self['Pipelines']['default'])
-                self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:YStarFilter')
-                self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:YBoostFilter')
-                self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:METSumEtFilter')
-                self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:GoodPrimaryVertexFilter')
-                self['Pipelines'][pipeline_name]['MinYStar'] = ys_lo
-                self['Pipelines'][pipeline_name]['MaxYStar'] = ys_hi
-                self['Pipelines'][pipeline_name]['MinYBoost'] = yb_lo
-                self['Pipelines'][pipeline_name]['MaxYBoost'] = yb_hi
-                # gen pipelines
-                if self.is_data is False:
-                    gen_pipeline_name = 'gen_yb{0}ys{1}'.format(i, j)
-                    self['Pipelines'][gen_pipeline_name] = copy.deepcopy(self['Pipelines']['gen_default'])
-                    self['Pipelines'][gen_pipeline_name]['Processors'].insert(0,'filter:GenYStarFilter')
-                    self['Pipelines'][gen_pipeline_name]['Processors'].insert(0,'filter:GenYBoostFilter')
-                    self['Pipelines'][gen_pipeline_name]['MinGenYStar'] = ys_lo
-                    self['Pipelines'][gen_pipeline_name]['MaxGenYStar'] = ys_hi
-                    self['Pipelines'][gen_pipeline_name]['MinGenYBoost'] = yb_lo
-                    self['Pipelines'][gen_pipeline_name]['MaxGenYBoost'] = yb_hi
+        for i, j in [(0,0), (0, 1), (0, 2), (1, 0), (1, 1), (2, 0)]:
+        # for i, j in [(1, 1)]:
+            yb_lo = self['RapidityAbsBinning'][i]
+            yb_hi = self['RapidityAbsBinning'][i+1]
+            ys_lo = self['RapidityAbsBinning'][j]
+            ys_hi = self['RapidityAbsBinning'][j+1]
+            # reco pipelines
+            pipeline_name = 'yb{0}ys{1}'.format(i, j)
+            self['Pipelines'][pipeline_name] = copy.deepcopy(self['Pipelines']['default'])
+            self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:YStarFilter')
+            self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:YBoostFilter')
+            self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:METSumEtFilter')
+            self['Pipelines'][pipeline_name]['Processors'].insert(0,'filter:GoodPrimaryVertexFilter')
+            self['Pipelines'][pipeline_name]['MinYStar'] = ys_lo
+            self['Pipelines'][pipeline_name]['MaxYStar'] = ys_hi
+            self['Pipelines'][pipeline_name]['MinYBoost'] = yb_lo
+            self['Pipelines'][pipeline_name]['MaxYBoost'] = yb_hi
+            # gen pipelines
+            if self.is_data is False:
+                gen_pipeline_name = 'gen_yb{0}ys{1}'.format(i, j)
+                self['Pipelines'][gen_pipeline_name] = copy.deepcopy(self['Pipelines']['gen_default'])
+                self['Pipelines'][gen_pipeline_name]['Processors'].insert(0,'filter:GenYStarFilter')
+                self['Pipelines'][gen_pipeline_name]['Processors'].insert(0,'filter:GenYBoostFilter')
+                self['Pipelines'][gen_pipeline_name]['MinGenYStar'] = ys_lo
+                self['Pipelines'][gen_pipeline_name]['MaxGenYStar'] = ys_hi
+                self['Pipelines'][gen_pipeline_name]['MinGenYBoost'] = yb_lo
+                self['Pipelines'][gen_pipeline_name]['MaxGenYBoost'] = yb_hi
 
