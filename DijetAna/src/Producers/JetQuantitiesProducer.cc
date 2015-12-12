3904af3b5488fd521a47e621419d77e630b78f7e
Georg Sieber <g.sieber@gmail.com>
2015-12-13 00:11:18 +0100

    switch from pointers to objects.

--- DijetAna/src/Producers/JetQuantitiesProducer.cc
+++ DijetAna/src/Producers/JetQuantitiesProducer.cc
@@ -5,69 +5,68 @@ std::string JetQuantitiesProducer::GetProducerId() const {
 }
 
 void JetQuantitiesProducer::Produce(JetEvent const& event, JetProduct& product, JetSettings const& settings) const {
-  auto& jetProduct = static_cast<JetProduct&>(product);
 
   // Inclusive Jets
-  for (auto i = product.m_validJets.begin(); i != product.m_validJets.end(); i++) {
-    jetProduct.m_incJetsPt.push_back((*i)->p4.Pt());
-    jetProduct.m_incJetsEta.push_back((*i)->p4.Eta());
-    jetProduct.m_incJetsRap.push_back((*i)->p4.Rapidity());
-    jetProduct.m_incJetsPhi.push_back((*i)->p4.Phi());
+  for (auto & jet : product.m_validRecoJets) {
+    product.m_incJetsPt.push_back(jet.p4.Pt());
+    product.m_incJetsEta.push_back(jet.p4.Eta());
+    product.m_incJetsRap.push_back(jet.p4.Rapidity());
+    product.m_incJetsPhi.push_back(jet.p4.Phi());
   }
 
-  jetProduct.m_jet1Pt = (jetProduct.m_validJets.size() > 0) ? jetProduct.m_validJets.at(0)->p4.Pt() : -999.;
-  jetProduct.m_jet1Eta = (jetProduct.m_validJets.size() > 0) ? jetProduct.m_validJets.at(0)->p4.Eta() : -999.;
-  jetProduct.m_jet1Rap = (jetProduct.m_validJets.size() > 0) ? jetProduct.m_validJets.at(0)->p4.Rapidity() : -999.;
-  jetProduct.m_jet1Phi = (jetProduct.m_validJets.size() > 0) ? jetProduct.m_validJets.at(0)->p4.Phi() : -999.;
+  product.m_jet1Pt = (product.m_validRecoJets.size() > 0) ? product.m_validRecoJets.at(0).p4.Pt() : -999.;
+  product.m_jet1Eta = (product.m_validRecoJets.size() > 0) ? product.m_validRecoJets.at(0).p4.Eta() : -999.;
+  product.m_jet1Rap = (product.m_validRecoJets.size() > 0) ? product.m_validRecoJets.at(0).p4.Rapidity() : -999.;
+  product.m_jet1Phi = (product.m_validRecoJets.size() > 0) ? product.m_validRecoJets.at(0).p4.Phi() : -999.;
 
-  jetProduct.m_jet2Pt = (jetProduct.m_validJets.size() > 1) ? jetProduct.m_validJets.at(1)->p4.Pt() : -999.;
-  jetProduct.m_jet2Eta = (jetProduct.m_validJets.size() > 1) ? jetProduct.m_validJets.at(1)->p4.Eta() : -999.;
-  jetProduct.m_jet2Rap = (jetProduct.m_validJets.size() > 1) ? jetProduct.m_validJets.at(1)->p4.Rapidity() : -999.;
-  jetProduct.m_jet2Phi = (jetProduct.m_validJets.size() > 1) ? jetProduct.m_validJets.at(1)->p4.Phi() : -999.;
+  product.m_jet2Pt = (product.m_validRecoJets.size() > 1) ? product.m_validRecoJets.at(1).p4.Pt() : -999.;
+  product.m_jet2Eta = (product.m_validRecoJets.size() > 1) ? product.m_validRecoJets.at(1).p4.Eta() : -999.;
+  product.m_jet2Rap = (product.m_validRecoJets.size() > 1) ? product.m_validRecoJets.at(1).p4.Rapidity() : -999.;
+  product.m_jet2Phi = (product.m_validRecoJets.size() > 1) ? product.m_validRecoJets.at(1).p4.Phi() : -999.;
 
-  jetProduct.m_dijet_mass = (jetProduct.m_validJets.size() > 1)
-                                ? (jetProduct.m_validJets.at(0)->p4 + jetProduct.m_validJets.at(1)->p4).mass()
+  product.m_dijet_mass = (product.m_validRecoJets.size() > 1)
+                                ? (product.m_validRecoJets.at(0).p4 + product.m_validRecoJets.at(1).p4).mass()
                                 : -999.;
-  jetProduct.m_dijet_ptavg = (jetProduct.m_validJets.size() > 1)
-                                 ? 0.5 * (jetProduct.m_validJets.at(0)->p4.Pt() + jetProduct.m_validJets.at(1)->p4.Pt())
+  product.m_dijet_ptavg = (product.m_validRecoJets.size() > 1)
+                                 ? 0.5 * (product.m_validRecoJets.at(0).p4.Pt() + product.m_validRecoJets.at(1).p4.Pt())
                                  : -999.;
-  jetProduct.m_dijet_ymax = (jetProduct.m_validJets.size() > 1) ? std::max(jetProduct.m_validJets.at(0)->p4.Rapidity(),
-                                                                           jetProduct.m_validJets.at(1)->p4.Rapidity())
+  product.m_dijet_ymax = (product.m_validRecoJets.size() > 1) ? std::max(product.m_validRecoJets.at(0).p4.Rapidity(),
+                                                                           product.m_validRecoJets.at(1).p4.Rapidity())
                                                                 : -999.;
-  jetProduct.m_dijet_yinner = (jetProduct.m_validJets.size() > 1)
-                                  ? ((std::abs(jetProduct.m_validJets.at(0)->p4.Rapidity()) <=
-                                      std::abs(jetProduct.m_validJets.at(1)->p4.Rapidity()))
-                                         ? jetProduct.m_validJets.at(0)->p4.Rapidity()
-                                         : jetProduct.m_validJets.at(1)->p4.Rapidity())
+  product.m_dijet_yinner = (product.m_validRecoJets.size() > 1)
+                                  ? ((std::abs(product.m_validRecoJets.at(0).p4.Rapidity()) <=
+                                      std::abs(product.m_validRecoJets.at(1).p4.Rapidity()))
+                                         ? product.m_validRecoJets.at(0).p4.Rapidity()
+                                         : product.m_validRecoJets.at(1).p4.Rapidity())
                                   : -999.;
-  jetProduct.m_dijet_youter = (jetProduct.m_validJets.size() > 1)
-                                  ? ((std::abs(jetProduct.m_validJets.at(0)->p4.Rapidity()) >
-                                      std::abs(jetProduct.m_validJets.at(1)->p4.Rapidity()))
-                                         ? jetProduct.m_validJets.at(0)->p4.Rapidity()
-                                         : jetProduct.m_validJets.at(1)->p4.Rapidity())
+  product.m_dijet_youter = (product.m_validRecoJets.size() > 1)
+                                  ? ((std::abs(product.m_validRecoJets.at(0).p4.Rapidity()) >
+                                      std::abs(product.m_validRecoJets.at(1).p4.Rapidity()))
+                                         ? product.m_validRecoJets.at(0).p4.Rapidity()
+                                         : product.m_validRecoJets.at(1).p4.Rapidity())
                                   : -999.;
-  jetProduct.m_dijet_deltaPhi =
-      (jetProduct.m_validJets.size() > 1)
-          ? std::abs(jetProduct.m_validJets.at(0)->p4.Phi() - jetProduct.m_validJets.at(1)->p4.Phi())
+  product.m_dijet_deltaPhi =
+      (product.m_validRecoJets.size() > 1)
+          ? std::abs(product.m_validRecoJets.at(0).p4.Phi() - product.m_validRecoJets.at(1).p4.Phi())
           : -999.;
-  jetProduct.m_dijet_jet12PtRatio =
-      (jetProduct.m_validJets.size() > 1)
-          ? jetProduct.m_validJets.at(1)->p4.Pt()/jetProduct.m_validJets.at(0)->p4.Pt()
+  product.m_dijet_jet12PtRatio =
+      (product.m_validRecoJets.size() > 1)
+          ? product.m_validRecoJets.at(1).p4.Pt()/product.m_validRecoJets.at(0).p4.Pt()
           : -999.;
-  jetProduct.m_dijet_cosThetaStar =
-      (jetProduct.m_validJets.size() > 1)
-          ? std::tanh(jetProduct.m_validJets.at(0)->p4.Rapidity() - jetProduct.m_validJets.at(1)->p4.Rapidity())
+  product.m_dijet_cosThetaStar =
+      (product.m_validRecoJets.size() > 1)
+          ? std::tanh(product.m_validRecoJets.at(0).p4.Rapidity() - product.m_validRecoJets.at(1).p4.Rapidity())
           : -999.;
-  jetProduct.m_dijet_yboost =
-      (jetProduct.m_validJets.size() > 1)
-          ? 0.5 * std::abs(jetProduct.m_validJets.at(0)->p4.Rapidity() + jetProduct.m_validJets.at(1)->p4.Rapidity())
+  product.m_dijet_yboost =
+      (product.m_validRecoJets.size() > 1)
+          ? 0.5 * std::abs(product.m_validRecoJets.at(0).p4.Rapidity() + product.m_validRecoJets.at(1).p4.Rapidity())
           : -999.;
-  jetProduct.m_dijet_ystar =
-      (jetProduct.m_validJets.size() > 1)
-          ? 0.5 * std::abs(jetProduct.m_validJets.at(0)->p4.Rapidity() - jetProduct.m_validJets.at(1)->p4.Rapidity())
+  product.m_dijet_ystar =
+      (product.m_validRecoJets.size() > 1)
+          ? 0.5 * std::abs(product.m_validRecoJets.at(0).p4.Rapidity() - product.m_validRecoJets.at(1).p4.Rapidity())
           : -999.;
-  jetProduct.m_dijet_chi =
-      (jetProduct.m_validJets.size() > 1)
-          ? exp(std::abs(jetProduct.m_validJets.at(0)->p4.Rapidity() - jetProduct.m_validJets.at(1)->p4.Rapidity()))
+  product.m_dijet_chi =
+      (product.m_validRecoJets.size() > 1)
+          ? exp(std::abs(product.m_validRecoJets.at(0).p4.Rapidity() - product.m_validRecoJets.at(1).p4.Rapidity()))
           : -999.;
 }
