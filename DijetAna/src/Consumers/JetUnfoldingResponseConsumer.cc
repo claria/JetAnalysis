6d075ea2cf64d722295b2cd7f8929feb111a3287
Georg Sieber <g.sieber@gmail.com>
2016-08-15 10:30:34 +0200

    rewrite unfolding response consumer and include fake/miss histograms.

--- DijetAna/src/Consumers/JetUnfoldingResponseConsumer.cc
+++ DijetAna/src/Consumers/JetUnfoldingResponseConsumer.cc
@@ -6,6 +6,13 @@ void JetUnfoldingResponseConsumer::Init(setting_type const& settings) {
 
   m_h1_unf_gen_ptavg = new TH1D("h_unf_gen_ptavg", "h_unf_gen_ptavg", settings.GetPtBinning().size() - 1, &settings.GetPtBinning()[0]);
   m_h1_unf_gen_ptavg->Sumw2();
+
+  m_h1_unf_fake_ptavg = new TH1D("h_unf_fake_ptavg", "h_unf_fake_ptavg", settings.GetPtBinning().size() - 1, &settings.GetPtBinning()[0]);
+  m_h1_unf_fake_ptavg->Sumw2();
+
+  m_h1_unf_miss_ptavg = new TH1D("h_unf_miss_ptavg", "h_unf_miss_ptavg", settings.GetPtBinning().size() - 1, &settings.GetPtBinning()[0]);
+  m_h1_unf_miss_ptavg->Sumw2();
+
   m_h1_unf_reco_ptavg = new TH1D("h_unf_reco_ptavg", "h_unf_reco_ptavg", settings.GetPtBinning().size() - 1, &settings.GetPtBinning()[0]);
   m_h1_unf_reco_ptavg->Sumw2();
   m_h2_unf_response_ptavg = new TH2D("h2_unf_response_ptavg","h2_unf_response_ptavg", settings.GetPtBinning().size() - 1, &settings.GetPtBinning()[0],
@@ -29,15 +36,51 @@ void JetUnfoldingResponseConsumer::ProcessFilteredEvent(event_type const& event,
     setting_type const& settings) {
   double eventWeight = product.m_weights.find(settings.GetEventWeight())->second;
 
-  m_h1_unf_gen_ptavg->Fill(product.m_gendijet_ptavg, eventWeight);
-  m_h1_unf_gen_idx->Fill(product.m_gendijet_idx, eventWeight);
 
-  if (product.m_genmatchdijet_ptavg > 133.) {
-    m_h1_unf_reco_ptavg->Fill(product.m_genmatchdijet_ptavg, eventWeight);
-    m_h1_unf_reco_idx->Fill(product.m_genmatchdijet_idx, eventWeight);
-    m_h2_unf_response_ptavg->Fill(product.m_genmatchdijet_ptavg, product.m_gendijet_ptavg, eventWeight);
-    m_h2_unf_response_idx->Fill(product.m_genmatchdijet_idx, product.m_gendijet_idx, eventWeight);
+  // check if valid gen event using tagging filters
+  bool validGenEvent = false;
+  if ((product.fres.GetDecisionEntry("NGenJetsFilter")->filterDecision == FilterResult::Decision::Passed) &&
+      (product.fres.GetDecisionEntry("GenPtAvgFilter")->filterDecision == FilterResult::Decision::Passed) &&
+      (product.fres.GetDecisionEntry("GenDijetsRapFilter")->filterDecision == FilterResult::Decision::Passed) &&
+      (product.fres.GetDecisionEntry("GenYStarFilter")->filterDecision == FilterResult::Decision::Passed) &&
+      (product.fres.GetDecisionEntry("GenYBoostFilter")->filterDecision == FilterResult::Decision::Passed)) 
+  {
+    validGenEvent = true;
   }
+
+  // check if valid reco event using tagging filters
+   bool validRecoEvent = false;
+   if ((product.fres.GetDecisionEntry("NJetsFilter")->filterDecision == FilterResult::Decision::Passed) &&
+       (product.fres.GetDecisionEntry("PtAvgFilter")->filterDecision == FilterResult::Decision::Passed) &&
+       (product.fres.GetDecisionEntry("DijetsRapFilter")->filterDecision == FilterResult::Decision::Passed) &&
+       (product.fres.GetDecisionEntry("YStarFilter")->filterDecision == FilterResult::Decision::Passed) &&
+       (product.fres.GetDecisionEntry("GoodPrimaryVertexFilter")->filterDecision == FilterResult::Decision::Passed) &&
+       (product.fres.GetDecisionEntry("METSumEtFilter")->filterDecision == FilterResult::Decision::Passed) &&
+       (product.fres.GetDecisionEntry("YBoostFilter")->filterDecision == FilterResult::Decision::Passed)) 
+   {
+     validRecoEvent = true;
+   }
+
+  if (validGenEvent) {
+    m_h1_unf_gen_ptavg->Fill(product.m_gendijet_ptavg, eventWeight);
+    m_h1_unf_gen_idx->Fill(product.m_gendijet_idx, eventWeight);
+  }
+  if (validRecoEvent) {
+    m_h1_unf_reco_ptavg->Fill(product.m_dijet_ptavg, eventWeight);
+    m_h1_unf_reco_idx->Fill(product.m_dijet_idx, eventWeight);
+  }
+  if (validGenEvent && !validRecoEvent) {
+    m_h1_unf_miss_ptavg->Fill(product.m_gendijet_ptavg, eventWeight);
+  }
+  if (validRecoEvent && !validGenEvent) {
+    m_h1_unf_fake_ptavg->Fill(product.m_dijet_ptavg, eventWeight);
+  }
+  if (validRecoEvent && validGenEvent) {
+    m_h2_unf_response_ptavg->Fill(product.m_dijet_ptavg, product.m_gendijet_ptavg, eventWeight);
+    m_h2_unf_response_idx->Fill(product.m_dijet_idx, product.m_gendijet_idx, eventWeight);
+  }
+  // m_genmatchdijet_ptavg
+
 }
 
 void JetUnfoldingResponseConsumer::Finish(setting_type const& settings) {
@@ -45,6 +88,8 @@ void JetUnfoldingResponseConsumer::Finish(setting_type const& settings) {
 
   m_h1_unf_gen_ptavg->Write();
   m_h1_unf_reco_ptavg->Write();
+  m_h1_unf_miss_ptavg->Write();
+  m_h1_unf_fake_ptavg->Write();
   m_h2_unf_response_ptavg->Write();
 
   m_h1_unf_gen_idx->Write();
