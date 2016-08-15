e5ea187e1f22a9b0def67020bd8d2302802d8558
Georg Sieber <g.sieber@gmail.com>
2016-08-15 10:29:46 +0200

    do not writeout dummy histogram.

--- DijetAna/src/Producers/GenJetQuantitiesProducer.cc
+++ DijetAna/src/Producers/GenJetQuantitiesProducer.cc
@@ -9,6 +9,7 @@ void GenJetQuantitiesProducer::Init(setting_type const& settings) {
   ProducerBase<JetTypes>::Init(settings);
   // dummy histogram used to find bin index
   m_h_ptavg = new TH1D("h_ptavg_dummy", "h_ptavg_dummy", settings.GetPtBinning().size() - 1, &settings.GetPtBinning()[0]);
+  m_h_ptavg->SetDirectory(0);
   m_h_ptavg->Sumw2();
 }
 
