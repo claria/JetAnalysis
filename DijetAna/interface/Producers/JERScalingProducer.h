bf44627d0807009516787650e54677734445a081
Georg Sieber <g.sieber@gmail.com>
2016-09-15 14:10:03 +0200

    update scaling producer.

--- DijetAna/interface/Producers/JERScalingProducer.h
+++ DijetAna/interface/Producers/JERScalingProducer.h
@@ -10,4 +10,24 @@ class JERScalingProducer : public JetProducerBase {
   double GetScalingFactor(double eta) const;
  private:
   std::vector<double> m_jerScalingFactors;
+  double maxDeltaR;
 };
+
+
+double get_sigma_rel(double pt, double eta);
+
+struct matchSort_deltaRdeltaPtVar
+{
+  matchSort_deltaRdeltaPtVar(const double dR = 0.3) : maxDeltaR(dR) {};
+
+  template<typename T1, typename T2>
+  double operator()(const T1 &obj1, const T2 &obj2) const
+  {
+    const double val1 = ROOT::Math::VectorUtil::DeltaR(obj1.p4, obj2.p4);
+    const double val2 = std::abs(obj1.p4.pt() - obj2.p4.pt()) / obj2.p4.pt();
+    // std::cout << "pt eta sigmarel delta " << obj2.p4.pt() << " " << obj2.p4.eta() << " " << get_sigma_rel(obj2.p4.pt(), obj2.p4.eta())<< " " << val2 << std::endl;
+    return (val1 < maxDeltaR && val2 < 1.5*get_sigma_rel(obj2.p4.pt(), obj2.p4.eta())) ? val1 : std::numeric_limits<double>::quiet_NaN();
+  }
+  const double maxDeltaR;
+};
+
--- DijetAna/src/Producers/JERScalingProducer.cc
+++ DijetAna/src/Producers/JERScalingProducer.cc
@@ -1,24 +1,75 @@
 #include "JetAnalysis/DijetAna/interface/Producers/JERScalingProducer.h"
 
+#include <KappaTools/RootTools/interface/Matching.h>
+
 std::string JERScalingProducer::GetProducerId() const {
   return "JERScalingProducer";
+}
+
+
+double get_sigma_rel(double pt, double eta) {
+  if (std::abs(eta) < 0.5)
+    return sqrt(std::pow(6.251,2)/std::pow(pt,2) + std::pow(1.062,2)/pt + std::pow(0.031,2));
+  if (std::abs(eta) < 1.1)
+    return sqrt(std::pow(6.710,2)/std::pow(pt,2) + std::pow(1.047,2)/pt + std::pow(0.036,2));
+  if (std::abs(eta) < 1.7)
+    return sqrt(std::pow(7.114,2)/std::pow(pt,2) + std::pow(1.119,2)/pt + std::pow(0.041,2));
+  if (std::abs(eta) < 2.3)
+    return sqrt(std::pow(7.845,2)/std::pow(pt,2) + std::pow(1.047,2)/pt + std::pow(1.08E-06,2));
+  if (std::abs(eta) < 2.8)
+    return sqrt(std::pow(7.330,2)/std::pow(pt,2) + std::pow(0.887,2)/pt + std::pow(1.58E-5,2));
+  if (std::abs(eta) < 3.2)
+    return sqrt(std::pow(6.389,2)/std::pow(pt,2) + std::pow(1.191,2)/pt + std::pow(1.818E-6,2));
+  else
+    return sqrt(std::pow(6.389,2)/std::pow(pt,2) + std::pow(1.191,2)/pt + std::pow(1.818E-6,2));
 }
 
 void JERScalingProducer::Init(JetSettings const& settings) {
   JetProducerBase::Init(settings);
   m_jerScalingFactors = settings.GetJERScalingFactors();
+  maxDeltaR = settings.GetMaxDeltaR();
   assert(m_jerScalingFactors.size() == 7);
 }
 
+
+
+
 void JERScalingProducer::Produce(JetEvent const& event, JetProduct& product, JetSettings const& settings) const {
-  for (auto & recojet : product.m_validRecoJets) {
-    double corr = this->GetScalingFactor(recojet.p4.Eta());
-    if (product.m_matchedGenJets.at(&recojet) != nullptr) {
-      recojet.p4.SetPt(std::max(0., product.m_matchedGenJets.at(&recojet)->p4.Pt() + corr * (recojet.p4.Pt() - product.m_matchedGenJets.at(&recojet)->p4.Pt())));
+  // for (auto & recojet : product.m_validRecoJets) {
+  //   double corr = this->GetScalingFactor(recojet.p4.Eta());
+  //   if (product.m_matchedGenJets.at(&recojet) != nullptr) {
+  //     recojet.p4.SetPt(std::max(0., product.m_matchedGenJets.at(&recojet)->p4.Pt() + corr * (recojet.p4.Pt() - product.m_matchedGenJets.at(&recojet)->p4.Pt())));
+  //   }
+  // }
+  // for (auto & genjet : product.m_validGenJets) {
+  //   double corr = this->GetScalingFactor(genjet.p4.Eta());
+  //   if (product.m_matchedRecoJets.at(&genjet) != nullptr) {
+  //     std::cout << "shifting from reco pt " << product.m_matchedRecoJets.at(&genjet)->p4.Pt() << std::endl;
+  //     product.m_matchedRecoJets.at(&genjet)->p4.SetPt(std::max(0., genjet.p4.Pt() + corr * (product.m_matchedRecoJets.at(&genjet)->p4.Pt() - genjet.p4.Pt())));
+  //     std::cout << "shifting to reco pt " << product.m_matchedRecoJets.at(&genjet)->p4.Pt() << std::endl;
+  //   }
+  // }
+  std::vector<KLV> genJets = *event.m_genJets;
+
+  static matchSort_deltaRdeltaPtVar metric(maxDeltaR);
+  std::vector<int> matchResult = matchSort_Matrix<KBasicJet, KLV>(product.m_corrJets, product.m_corrJets.size(), 
+                                                                         genJets, genJets.size(), 
+                                                                         metric);
+
+  for (auto genJet = event.m_genJets->begin(); genJet != event.m_genJets->end(); ++genJet) {
+    size_t idx = genJet - event.m_genJets->begin();
+
+    if (matchResult[idx] < 0) {
+      continue;
+    }
+    else {
+      double shift = this->GetScalingFactor((*genJet).p4.Eta());
+      product.m_corrJets.at(matchResult[idx]).p4.SetPt(std::max(0., genJet->p4.Pt() + shift*(product.m_corrJets.at(matchResult[idx]).p4.Pt() - genJet->p4.Pt())));
     }
   }
-  std::sort(product.m_validRecoJets.begin(),
-            product.m_validRecoJets.end(),
+
+  std::sort(product.m_corrJets.begin(),
+            product.m_corrJets.end(),
             [](KBasicJet jet1, KBasicJet jet2)
                 -> bool { return jet1.p4.Pt() > jet2.p4.Pt(); });
 
@@ -48,7 +99,6 @@ double JERScalingProducer::GetScalingFactor(double eta) const {
     return m_jerScalingFactors.at(6);
   }
   else {
-    LOG(FATAL) << "WARNING. Jet outside |eta| < 5" << std::endl;
     return 1.0;
   }
 }
